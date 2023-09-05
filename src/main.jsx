368af5f7db57a92bd0f02ca347e36817d823c489
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-09-04 22:20:46 -0300

    Importando elementos do router e instalação do DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home/index.jsx';
+import Aparelhos from './routes/Aparelhos/index.jsx';
+import VisualizarAparelho from './routes/VisualizarAparelho/index.jsx';
+import Error from './routes/Error/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/' ,
+    element: <App/> ,
+    errorElement: <Error/> ,
+    children: [
+      { path: '/' ,
+      element: <Home/>
+      },
+      {path: '/Aparelhos' ,
+    element:<Aparelhos/>
+  },
+      {path: '/VisualizarAparelho' ,
+      element: <VisualizarAparelho/>
+
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
