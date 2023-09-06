a90150c51f7fb210a5ffb11cd194c5c6bfe9a495
LoenardoCamargo77 <leonardocamargx@gmail.com>
2023-09-05 22:27:52 -0300

    Quase completo.

---
+++ src/components/ListaProdutos.js
@@ -0,0 +1,20 @@
+export const visualizarProdutos = [
+    {
+        id: 1,
+        nome: 'Celular',
+        preco: 1500
+    },
+    {
+        id: 2,
+        nome: 'Celular 2',
+        preco: 1800
+    },
+    {
+        id: 3,
+        nome: 'Celular 3',
+        preco: 1850
+    },
+
+
+
+]
--- src/routes/Aparelhos/index.jsx
+++ src/routes/Aparelhos/index.jsx
@@ -1,9 +1,11 @@
+
 export default function Aparelhos(){
     
     return(
         <main>
             <h1>Aparelhos Disponíveis</h1>
         </main>
+    
     )
 
 
